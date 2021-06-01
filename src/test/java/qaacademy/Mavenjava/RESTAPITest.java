036f8b51b62018a764e659217b3f2062a8a936dc
tringapps <pooja.attri@tringapps.com>
2021-06-01 16:22:31 +0530

    FIRST COMMIT

---
+++ src/test/java/qaacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,16 @@
+package qaacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+@Test
+public void DeleteTwitter()
+{
+	System.out.println("DeleteTwitter");
+}
+}
